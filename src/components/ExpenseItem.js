05b6ad0dee15fff824f40cf888c93aa0089d6b1a
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-17 12:40:39 +0100

    add event on button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,17 @@
-import ExpenseDate from './ExpenseDate'
-import Card from './Card'
+import React, {useState} from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+   
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        
+    }
 // const  expenseDate= new Date(2022,10,1);
 // const expenseTitle = 'Car Insurance';
 // const expenseAmount = 259.99
@@ -10,9 +19,10 @@ function ExpenseItem(props) {
         <Card className='expense-item'>
            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.ammount}</div>
             </div>
+            <button onClick={clickHandler}> title</button>
         </Card>
         )
 }
