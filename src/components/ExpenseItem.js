f4fcc6bdbd32b148f73150137572d640f5b1c59f
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-17 11:23:26 +0100

    splitting style into a new component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,20 @@
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 // const  expenseDate= new Date(2022,10,1);
 // const expenseTitle = 'Car Insurance';
 // const expenseAmount = 259.99
-
-
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.ammount}</div>
             </div>
-        </div>)
+        </Card>
+        )
 }
 
 export default ExpenseItem;
