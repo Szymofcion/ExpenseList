008651b84d129320ed0981042d76df5a4e754fe2
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-17 15:34:02 +0100

    add ExpenseForm section

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [{
     title: 'Toilet Paper',
     amount: 45.12,
     date: new Date(2022, 4, 12)
-},
-{
+  },
+  {
     title: 'food',
     amount: 341.23,
     date: new Date(2022, 11, 16)
-},
-{
+  },
+  {
     title: 'Car Insurance',
     amount: 299.99,
     date: new Date(2022, 10, 12)
-},
-{
+  },
+  {
     title: 'Gifts ',
     amount: 95.42,
     date: new Date(2022, 12, 1)
-}
-];
+  }
+  ];
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}/>
+      <NewExpense/>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+import ExpenseDate from '../Expenses/ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+   
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        
+    }
+// const  expenseDate= new Date(2022,10,1);
+// const expenseTitle = 'Car Insurance';
+// const expenseAmount = 259.99
+    return (
+        <Card className='expense-item'>
+           <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${props.ammount}</div>
+            </div>
+            <button onClick={clickHandler}> title</button>
+        </Card>
+        )
+}
+
+export default ExpenseItem;
