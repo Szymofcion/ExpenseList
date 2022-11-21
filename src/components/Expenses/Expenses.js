40bf1b662dc25b8353d0294aee564fabdf17ab9e
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-21 13:38:44 +0100

    add filterChangeHandler and custom component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,29 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
-import ExpenseFilter from './ExpenseFilter';
+import ExpensesFilter from './ExpensesFilter'
 
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+
+    };
 
     return (
+
         <Card className='expenses'>
-            <ExpenseFilter/>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             <ExpenseItem title={props.items[0].title} ammount={props.items[0].amount} date={props.items[0].date} />
             <ExpenseItem title={props.items[1].title} ammount={props.items[1].amount} date={props.items[1].date} />
             <ExpenseItem title={props.items[2].title} ammount={props.items[2].amount} date={props.items[2].date} />
             <ExpenseItem title={props.items[3].title} ammount={props.items[3].amount} date={props.items[3].date} />
         </Card>
+
     )
 }
 
