ce55e491a27c5ced59a081363202966c89454f37
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-23 20:16:44 +0100

    add filter year

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import NewExpense from './components/NewExpense/NewExpense';
 
 const DUMMY_EXPENSES = [
   {
+    id:'1',
     title: 'Toilet Paper',
     amount: 45.12,
     date: new Date(2022, 4, 12)
   },
   {
+    id:'2',
     title: 'food',
     amount: 341.23,
     date: new Date(2022, 11, 16)
   },
   {
+    id:'3',
     title: 'Gifts ',
     amount: 95.42,
     date: new Date(2022, 12, 1)
   },
   {
+    id:'4',
     title: 'Car Insurance',
     amount: 299.99,
     date: new Date(2022, 10, 12)
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,17 +3,20 @@ import React from "react";
 import './ExpensesFilter.css'
 
 const ExpensesFilter = (props) => {
+
+
   const filterChangeHandler = (e) => {
     props.onChangeFilter(e.target.value)
   }
+  
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={filterChangeHandler}>
-          <option value='2023'>2023</option>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
+          <option  value='2023'>2023</option>
+          <option  value='2022'>2022</option>
+          <option  value='2021'>2021</option>
         </select>
       </div>
     </div>
