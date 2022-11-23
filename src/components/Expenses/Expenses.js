be3d63cd2e7cd9ea22b9e18056898a8b46868338
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-23 21:18:07 +0100

    add conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,15 +13,17 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
-const filteredExpenses = props.items.filter(expense =>{
-    return expense.date.getFullYear().toString()=== filteredYear;
-})
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
 
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.map((expense) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))}
+                {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+                {filteredExpenses.length > 0 &&
+                    filteredExpenses.map((expense) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))}
             </Card>
         </div>
     )
