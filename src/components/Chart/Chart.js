8f13103ae3b065acaf3ad6de9758ddb5e67cf4ed
Szymofcion <Szymon.dziewajs@gmail.com>
2022-11-25 11:05:22 +0100

    complete chartBar git add .!

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,11 +5,14 @@ import ChartBar from './ChartBar'
 import './Chart.css';
 
 const Chart = props => {
+    const valueArray = props.dataPoints.map(dataPoint => dataPoint.value);
+    const maxValue = Math.max(...valueArray)
+
 
     return (
         <div className="chart">
             {props.dataPoints.map((dataPoint) => (
-            <ChartBar key={dataPoint.id } value={dataPoint.value} maxValue={null} label={dataPoint.label} />)) }
+                <ChartBar key={dataPoint.id} value={dataPoint.value} maxValue={maxValue} label={dataPoint.label} />))}
         </div>
     )
 }
